2429253b30b08befdc252d1684351f16bfba7d98
HTMLhead <mrdoo309@gmail.com>
2019-04-10 16:55:29 +0900

    Feat: cors접근 허용 기능 추가

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
-const express = require("express");
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const indexRouter  = require('./routes/index.js');
-const usersRouter  = require('./routes/users.js');
+const indexRouter = require('./routes/index.js');
+const usersRouter = require('./routes/users.js');
 
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
+  useNewUrlParser: true,
 });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
-db.once('open', () => console.log("Connected to Mongoose"));
+db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
