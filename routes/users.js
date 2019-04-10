35b1acc8862ad3e106c79056cc4ecebe7b9c0372
Kim <a01088570194@gmail.com>
2019-04-10 14:56:19 +0900

    Feat: signup POST router 추가

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../model/user.js");
 
 router.get('/', function (req, res) {
     res.send('Hello');
 })
 router.post('/signin', (req, res) => {
-    
+
 })
-router.post('/signup', (req, res) => {
-    
+router.post('/signup', async (req, res) => {
+    const user = new User({
+        userId: req.body.id,
+        password: req.body.password,
+        username: req.body.name
+    })
+    try {
+        const newUser = await user.save();
+        res.redirect('/');
+    } catch {
+        res.json({error: 'signup error'})
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const indexRouter  = require('./routes/index.js');
+const usersRouter  = require('./routes/users.js');
 
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.listen(process.env.PORT || 3000);
