0c25e2e7b4ff1f7e0dabcc4ba7d7bf6d269d2c67
HTMLhead <mrdoo309@gmail.com>
2019-04-11 16:07:51 +0900

    Style: indent 조정

--- routes/users.js
+++ routes/users.js
@@ -3,74 +3,80 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require("../model/user.js");
+const User = require('../model/user.js');
 
 function createToken(user, res, next) {
-    const payload = {
-      userid: user.userid,
-      username: user.username
-    };
-  
-    jwt.sign(payload, process.env.TOKEN_SECRET, {
-      expiresIn: '1d'
-    }, (err, token) => {
+  const payload = {
+    userid: user.userid,
+    username: user.username,
+  };
+
+  jwt.sign(
+    payload,
+    process.env.TOKEN_SECRET,
+    {
+      expiresIn: '1d',
+    },
+    (err, token) => {
       if (err) {
         res.json({
-					error: 'token 생성 에러'
-				})
+          error: 'token 생성 에러',
+        });
       } else {
         res.json({
-          token
+          token,
         });
       }
-    });
-  }
+    }
+  );
+}
 
-router.get('/', function (req, res) {
-    res.send('Hello');
-})
+router.get('/', function(req, res) {
+  res.send('Hello');
+});
 router.post('/signin', async (req, res) => {
-    let searchOptions = {};
-    if(req.body.userid !== null && req.body.userid !== ''){
-        searchOptions.userid = req.body.userid;
-        searchOptions.password = req.body.password;
+  let searchOptions = {};
+  if (req.body.userid !== null && req.body.userid !== '') {
+    searchOptions.userid = req.body.userid;
+    searchOptions.password = req.body.password;
+  }
+  try {
+    const user = await User.findOne(searchOptions);
+    if (user) {
+      const bMatch = await bcrypt.compare(req.body.password, user.password);
+      if (bMatch) {
+        createToken(user, res, next);
+        res.json({
+          done: 'signin',
+          author: author,
+        });
+      } else {
+        res.json({ error: '비밀번호가 틀립니다.' });
+      }
     }
-    try {
-        const user = await User.findOne(searchOptions);
-        if(user){
-            const bMatch = await bcrypt.compare(req.body.password, user.password);
-            if(bMatch){
-                createToken(user, res, next);
-                res.json({
-                    "done": "signin",
-                    author: author
-                });
-            } else {
-                res.json({ error: '비밀번호가 틀립니다.'})
-            }
-        }
-    } catch {
-        res.json({"err": "signin error"});
-    }
-})
+  } catch {
+    res.json({ err: 'signin error' });
+  }
+});
+
 router.post('/signup', async (req, res) => {
-    try {
-        const user = await User.findOne({ userid: req.body.userid });
-        if(user) {
-            res.json({error: '중복된 아이디가 있습니다.'});
-        } else {
-            const hashedPassword = await bcrypt.hash(req.body.password.trim(), 12);
-            const user = new User({
-                userid: req.body.userid,
-                password: hashedPassword,
-                username: req.body.username
-            });
-            const newUser = await user.save();
-            createToken(newUser, res, next);
-        }
-    }  catch  {
-        res.json({ error: "signin error"});
+  try {
+    const user = await User.findOne({ userid: req.body.userid });
+    if (user) {
+      res.json({ error: '중복된 아이디가 있습니다.' });
+    } else {
+      const hashedPassword = await bcrypt.hash(req.body.password.trim(), 12);
+      const user = new User({
+        userid: req.body.userid,
+        password: hashedPassword,
+        username: req.body.username,
+      });
+      const newUser = await user.save();
+      createToken(newUser, res, next);
     }
-})
+  } catch {
+    res.json({ error: 'signin error' });
+  }
+});
 
 module.exports = router;
