2b9bccfcdb35dc42b4c432d936509c28089a115f
HTMLhead <mrdoo309@gmail.com>
2019-04-18 17:30:00 +0900

    Style: 세미콜론, 띄어쓰기 indent수정

--- routes/creator.js
+++ routes/creator.js
@@ -7,33 +7,33 @@ const User = require('../model/user.js');
 
 router.post('/edit', async (req, res) => {
   try {
-    const query = ({ userid: req.user.userid })
+    const query = { userid: req.user.userid };
     console.log(query);
-    const updatedData = ({
+    const updatedData = {
       userimage: req.body.userimage,
       userdesc: req.body.userdesc,
       username: req.body.username,
-    })
+    };
     const user = await User.findOneAndUpdate(query, updatedData);
-    res.json(updatedData)
-  } catch {
-    res.sendStatus(500).send('서버단 에러 발생')
-  }
-});
-
-router.post('/', async(req, res) => {
-  try {
-    const query = ({ userid: req.user.userid})
-    const user = await User.findOne(query)
-    const creatorData = ({
-      userimage: user.userimage,
-      userdesc: user.userdesc,
-      username: user.username,
-    })
-    res.json( creatorData )
+    res.json(updatedData);
   } catch (err) {
     res.send(err.message);
   }
-})
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const query = { userid: req.user.userid };
+    const user = await User.findOne(query);
+    const creatorData = {
+      userimage: user.userimage,
+      userdesc: user.userdesc,
+      username: user.username,
+    };
+    res.json(creatorData);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
 module.exports = router;
