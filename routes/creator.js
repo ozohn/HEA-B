e629025cb967a78868824a47b04cbae01344c445
HTMLhead <mrdoo309@gmail.com>
2019-04-18 16:59:26 +0900

    Refactor: 필요없는 괄호 제거

--- routes/creator.js
+++ routes/creator.js
@@ -25,11 +25,11 @@ router.post('/', async(req, res) => {
   try {
     const query = ({ userid: req.user.userid})
     const user = await User.findOne(query)
-    const creatorData = ({
+    const creatorData = {
       userimage: user.userimage,
       userdesc: user.userdesc,
       username: user.username,
-    })
+    }
     res.json( creatorData )
   } catch (err) {
     res.send(err.message);
