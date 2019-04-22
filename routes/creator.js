895016dc8762dc0c74ab6b12cfe71611480d6540
Kim <a01088570194@gmail.com>
2019-04-22 14:40:51 +0900

    Fix: merge 해결

--- routes/creator.js
+++ routes/creator.js
@@ -1,18 +1,17 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../model/user.js');
+const User = require("../model/user.js");
 
-router.post('/edit', async (req, res) => {
+router.post("/edit", async (req, res) => {
   try {
     const query = { userid: req.user.userid };
-    console.log(query);
     const updatedData = {
       userimage: req.body.userimage,
       userdesc: req.body.userdesc,
-      username: req.body.username,
+      username: req.body.username
     };
     const user = await User.findOneAndUpdate(query, updatedData);
     res.json(updatedData);
@@ -21,14 +20,14 @@ router.post('/edit', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const query = { userid: req.user.userid };
     const user = await User.findOne(query);
     const creatorData = {
       userimage: user.userimage,
       userdesc: user.userdesc,
-      username: user.username,
+      username: user.username
     };
     res.json(creatorData);
   } catch (err) {
