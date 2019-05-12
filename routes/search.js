88d6f6194a4089c2af78e6568f9fa059e974ccfa
HTMLhead <mrdoo309@gmail.com>
2019-05-12 21:47:27 +0900

    Feat: 작품 검색 경로 구현

--- routes/search.js
+++ routes/search.js
@@ -7,6 +7,7 @@ const _u = require('../util.js');
 
 router.post('/author', async (req, res) => {
   try {
+    console.log(req.body);
     const pattern = new RegExp(req.body.inputValue);
     const user = await User.find(
       { username: pattern },
@@ -14,6 +15,23 @@ router.post('/author', async (req, res) => {
     );
     if (user) {
       res.json(user);
+    } else {
+      res.status(404).send({ message: '일치하는 정보 없음' });
+    }
+  } catch {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/work', async (req, res) => {
+  try {
+    const pattern = new RegExp(req.body.inputValue);
+    const work = await Work.find(
+      { worktitle: pattern },
+      { worktitle: true, workimage: true, workdesc: true }
+    );
+    if (work) {
+      res.json(work);
     } else {
       res.status(404).send({ message: '일치하는 정보 없음' });
     }
