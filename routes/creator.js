c2054b742a9859899a7149f6a9eee0ac2c9638bb
HTMLhead <mrdoo309@gmail.com>
2019-05-17 11:55:41 +0900

    Refactor: console제거

--- routes/creator.js
+++ routes/creator.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../model/user.js");
-const Work = require("../model/work.js");
-const _u = require("../util.js");
+const User = require('../model/user.js');
+const Work = require('../model/work.js');
+const _u = require('../util.js');
 
-router.post("/edit", async (req, res) => {
+router.post('/edit', async (req, res) => {
   try {
     const query = _u.getQurey(req);
     const updatedData = _u.updateData(req.body);
@@ -15,7 +15,7 @@ router.post("/edit", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const query = _u.getQurey(req);
     const user = await User.findOne(query);
