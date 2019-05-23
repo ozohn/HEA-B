c63144c1ec3f1cfe64cf86fb4879608e6c6917f2
pdvonzoo <a01088570194@gmail.com>
2019-05-24 02:28:31 +0900

    Feat: graphQL schema 정의

--- src/schema.js
+++ src/schema.js
@@ -1,2 +1,13 @@
-import { makeExcutableSchema } from "graphql-tools";
+import path from "path";
+import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
+
+const schema = makeExecutableSchema({
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
+});
+
+export default schema;
