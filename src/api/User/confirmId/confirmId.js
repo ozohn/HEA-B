de3da8c14c2ed76da37bc6964310d9bc00182755
pdvonzoo <a01088570194@gmail.com>
2019-05-27 03:53:53 +0900

    Feat: SignUp form 관련 resolve, query 재정의

--- src/api/User/confirmId/confirmId.js
+++ src/api/User/confirmId/confirmId.js
@@ -1,12 +1,9 @@
 import { User } from "../../../../model/user";
 
 export default {
-  Query: {
-    currentUser: async (_, args) => {
-      const query = { userid };
-      const error = await User.findOne({ userid: "hsens00" });
-      if (error) return true;
-      return false;
+  Mutation: {
+    confirmId: async (_, args) => {
+      return (await User.findOne({ userid: args.userid })) ? true : false;
     }
   }
 };
