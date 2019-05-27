3157e6ada4fd210f4817eb0c95f3a98818aaa5e0
HTMLhead <mrdoo309@gmail.com>
2019-05-27 15:28:47 +0900

    Feat: middleware구현

--- src/server.js
+++ src/server.js
@@ -3,9 +3,25 @@ dotenv.config();
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
+import { checkToken, isLoggedIn } from './middleware';
+// import passport from './middleware';
+// console.log(passport);
+// passport.initialize();
 
 const PORT = process.env.PORT || 5000;
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+  schema,
+  context: ({ request, response }) => {
+    const check = checkToken(request, response);
+    console.log(check);
+    // request.headers.authorization;
+    // passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    //   if (user) {
+    //     request.user = user;
+    //   }
+    // })(request);
+  }
+});
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
