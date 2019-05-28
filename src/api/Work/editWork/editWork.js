504e9c4cbf3b7cc3769312483ce2cc1200708113
pdvonzoo <a01088570194@gmail.com>
2019-05-29 01:24:39 +0900

    Feat: edit work

--- src/api/Work/editWork/editWork.js
+++ src/api/Work/editWork/editWork.js
@@ -1,4 +1,5 @@
 import { Work } from "../../../../model/work";
+import { User } from "../../../../model/user";
 
 const DELETE = "DELETE";
 const EDIT = "EDIT";
@@ -7,13 +8,22 @@ export default {
   Mutation: {
     editWork: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated({ request });
-      const { worktitle, workdesc, workimage, action } = args;
+      const { workid, worktitle, workdesc, workimage, action } = args;
       const updateData = { worktitle, workdesc, workimage };
-      const query = { workid };
+      const query = { _id: workid };
       if (action === EDIT) {
-        return await Work.findOneAndUpdate(query, {
-          $set: { worktitle, workdesc, workimage }
+        await Work.findOneAndUpdate(
+          { _id: workid },
+          {
+            $set: { worktitle, workdesc, workimage }
+          }
+        );
+        await User.findById(request.user.id, async (err, user) => {
+          const work = user.works.id(workid);
+          work.set({ worktitle, workdesc, workimage });
+          await user.save();
         });
+        return updateData;
       } else if (action === DELETE) {
         return await User.findOneAndRemove(query);
       }
