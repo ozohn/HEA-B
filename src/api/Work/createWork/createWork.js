43cfaa742aaf3061c5131677fe1019baa2465864
HTMLhead <mrdoo309@gmail.com>
2019-05-28 20:49:30 +0900

    Refactor: work를 생성할때 사용자의 work배열과 works데이터베이스에 저장되도록 수정

--- src/api/Work/createWork/createWork.js
+++ src/api/Work/createWork/createWork.js
@@ -1,14 +1,24 @@
-import { User } from "../../../../model/user";
+import { User } from '../../../../model/user';
+import { Work } from '../../../../model/work';
 
 export default {
   Mutation: {
     createWork: async (_, args, { request, isAuthenticated }) => {
-      // isAuthenticated(request);
-      const query = { userid: args.userid };
-      const { worktitle, workdesc, workimage } = args;
-      return await User.findOneAndUpdate(query, {
-        $set: { works: { worktitle, workimage, workdesc } }
+      isAuthenticated({ request });
+      const { worktitle, workdesc, workimage, userid } = args;
+      const work = new Work({
+        worktitle: worktitle,
+        workdesc: workdesc,
+        workimage: workimage,
+        userid: userid
       });
+      const query = { userid: userid };
+      await work.save();
+      const user = await User.findOne(query);
+      await User.findOneAndUpdate(query, {
+        $set: { works: [...user.works, work] }
+      });
+      return work;
     }
   }
 };
