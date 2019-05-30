4f45e3c1da51743af1dd65ddc44c613ee9fc3efa
pdvonzoo <a01088570194@gmail.com>
2019-05-30 16:58:41 +0900

    Fix: work delete 버그

--- src/api/Work/deleteWork/deleteWork.js
+++ src/api/Work/deleteWork/deleteWork.js
@@ -1,11 +1,18 @@
+import { User } from "../../../../model/user";
 import { Work } from "../../../../model/work";
 
 export default {
   Mutation: {
-    deleteWork: async (_, args) => {
-      const query = { _id: args.workid };
+    deleteWork: async (_, args, { request }) => {
+      const { workid } = args;
+      const query = { _id: workid };
       const work = await Work.findOneAndRemove(query);
-      if (work) return true;
+      const user = await User.findById(request.user.id, async (err, user) => {
+        const work = user.works.id(workid);
+        work.remove();
+        await user.save();
+      });
+      if (work && user) return true;
       return false;
     }
   }
--- src/api/Work/seeWork/seeWork.js
+++ src/api/Work/seeWork/seeWork.js
@@ -1,7 +1,7 @@
 import { Work } from "../../../../model/work";
 
 export default {
-  Query: {
+  Mutation: {
     seeWork: async (_, args) => {
       const query = { _id: args.workid };
       return await Work.findOne(query);
